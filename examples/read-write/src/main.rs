6558a56cc9482c2e7d5bd170581255c3e6fae237
Daiki Ueno <dueno@redhat.com>
2020-11-21 16:22:11 +0100

    examples: Check the actual bytes after read

--- examples/read-write/src/main.rs
+++ examples/read-write/src/main.rs
@@ -5,15 +5,16 @@ use std::os::wasi::prelude::FromRawFd;
 
 use wasi_memory;
 
+const DATA: &[u8] = b"Hello world!";
+
 fn test_read_write() {
     // Open a file descriptor backed by memory
     let mem_fd = unsafe { wasi_memory::open().expect("opening memory") };
 
     // Write data to the file descriptor
     let mut file = unsafe { std::fs::File::from_raw_fd(mem_fd) };
-    let data = b"Hello world!";
-    let nwritten = file.write(data).expect("writing to a file");
-    assert_eq!(nwritten, data.len());
+    let nwritten = file.write(DATA).expect("writing to a file");
+    assert_eq!(nwritten, DATA.len());
 
     // Seek to the beginning of the file
     let offset = file.seek(std::io::SeekFrom::Start(0)).expect("seeking to the beginning of the file");
@@ -25,7 +26,7 @@ fn test_read_write() {
     // Read data from the file descriptor
     let mut buffer = String::new();
     file.read_to_string(&mut buffer).expect("reading file");
-    assert_eq!(buffer.len(), data.len());
+    assert_eq!(buffer.as_bytes(), DATA);
 
     println!("{}", buffer);
 }
